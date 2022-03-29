2a405b079d3a7a1ec3d98780bedafb1c48641aba
quinndecker <quinndecker@gmail.com>
2022-03-29 15:30:12 -0500

    security fixes and mobile formatting

--- DCC_site_2/settings.py
+++ DCC_site_2/settings.py
@@ -23,7 +23,7 @@ TEMPLATES_DIRS = os.path.join(BASE_DIR,'templates')
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
